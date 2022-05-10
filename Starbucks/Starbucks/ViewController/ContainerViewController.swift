49e0ab1cf1e766efb7323ed388653c0cf008b56b
asqw887 <asqw887@gmail.com>
2022-05-10 11:29:35 +0900

    feat: 날짜 비교 메소드

--- Starbucks/Starbucks/ViewController/ContainerViewController.swift
+++ Starbucks/Starbucks/ViewController/ContainerViewController.swift
@@ -12,4 +12,24 @@ class ContainerViewController: UITabBarController {
         super.viewDidLoad()
     }
 
+    func showEventModal() {
+
+    }
+    
+    func checkLastLogin(){
+        let now = Date()
+
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy/MM/dd HH:mm"
+        let past = formatter.date(from: "2021/05/10 22:31")
+
+        let diff = Calendar.current.dateComponents([.day], from: now, to: past ?? Date())
+        if diff.day == 0 {
+            // UserDefault 값 확인
+            UserDefaults.standard.set(now, forKey: "lastLogin")
+        }
+        else {
+            }
+    }
+
 }
