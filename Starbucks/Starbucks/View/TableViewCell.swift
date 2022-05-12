8f277f7a55e27ff499b11c4a68c29795704313d8
asqw887 <asqw887@gmail.com>
2022-05-12 17:00:25 +0900

    feat : CollectionCustomCell 구현 #8

---
+++ Starbucks/Starbucks/View/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  Starbucks
+//
+//  Created by 최예주 on 2022/05/12.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Starbucks/Starbucks/ViewController/CollectionViewController.swift
@@ -0,0 +1,43 @@
+//
+//  CollectionViewController.swift
+//  Starbucks
+//
+//  Created by 최예주 on 2022/05/12.
+//
+
+import UIKit
+
+class CollectionViewController: UIViewController {
+
+    @IBOutlet var collectionView: UICollectionView!
+    let data = ["9200000002760", "25", "9200000002487", "9300000003067", "9300000003524"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        registerCell()
+
+    }
+    func registerCell() {
+        let nibName = UINib(nibName: "RecommendItemCollectionViewCell", bundle: .main)
+        collectionView.register(nibName, forCellWithReuseIdentifier: "ItemCell")
+    }
+
+}
+extension CollectionViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return data.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ItemCell", for: indexPath) as? RecommendItemCollectionViewCell else { return UICollectionViewCell()}
+
+        cell.itemImageView.image = UIImage(systemName: "x.circle")
+        cell.nameLabel.text = data[indexPath.item]
+
+        return cell
+
+    }
+
+}
